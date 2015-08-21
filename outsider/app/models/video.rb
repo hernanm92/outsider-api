e8f3fae614b0596066adfa4c5a2aee80ff9be43c
Hernan Maschwitz <hernanm992@gmail.com>
2015-08-21 00:44:06 -0300

    video resource

--- outsider/app/models/video.rb
+++ outsider/app/models/video.rb
@@ -1,2 +1,10 @@
 class Video < ActiveRecord::Base
+	after_initialize :set_defaults
+
+	private
+
+	def set_defaults
+		self.views ||= 0
+		self.points ||= 0.0 #cuando se haga un update va a haber que validar que cada uno vote una vez (y hacer el promedio)
+	end
 end

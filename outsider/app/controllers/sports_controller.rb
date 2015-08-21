6bbafbd21e4ff355460daf1bb0d1f32e4531c1ca
Hernan Maschwitz <hernanm992@gmail.com>
2015-08-21 02:03:15 -0300

    sport resource

--- outsider/app/controllers/sports_controller.rb
+++ outsider/app/controllers/sports_controller.rb
@@ -1,8 +1,36 @@
 class SportsController < ApplicationController
 
-def index
-	@sports = Sport.first
+  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
+  #porque tengo que poner esto? validacion de rails para evitar CSRF attacks
+
+  def index
+    @sports = Sport.all
     render json: @sports
-end
+  end
+
+  def show
+    @sport = Sport.find_by!(sport_find_params)
+    render json: @sport
+  end
+
+  def create
+    @sport = Sport.create!(sport_create_params)
+    render json: @sport
+  end
+
+  private
+
+  def sport_create_params
+    {
+      name: params.require(:name),
+      description: params.require(:description)
+    }
+  end
+
+  def sport_find_params
+    {
+      name: params.require(:id)
+    }
+  end
 
 end

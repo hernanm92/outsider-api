8b6dd439cf302a5f05da1b21657cd2dd810d0b29
Hernan Maschwitz <hernanm992@gmail.com>
2015-09-16 00:13:01 -0300

    remove validation private field in user

--- outsider/app/models/user.rb
+++ outsider/app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   validates :alias, presence: true, uniqueness: true, length: { in: 5..15 }
   validates :name, presence: true, length: { in: 5..30 }
   validates :password, presence: true, length: { in: 5..30 }
-  validates :private, presence: true
+  #validates :private, presence: true
 
   def self.find_by!(params)
     params[:alias] = params.delete(:id) if params[:id]

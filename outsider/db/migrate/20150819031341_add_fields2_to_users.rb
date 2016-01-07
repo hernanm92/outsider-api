a593b8a1812f2ebbdfc177a6690b0e6fb7991065
Hernan Maschwitz <hernanm992@gmail.com>
2016-01-07 02:44:18 -0300

    fix migrations

--- outsider/db/migrate/20150819031341_add_fields2_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddFields2ToUsers < ActiveRecord::Migration
-  def change
-    #change_column :users, :followers,  :string
-    #remove_column :users, :followers
-    drop_table :users
-  end
-end

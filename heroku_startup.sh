35838427c2badd4627be5f7d47163d4e25e14281
Hernan Maschwitz <hernanm992@gmail.com>
2016-01-07 22:46:25 -0300

    environment variables

--- heroku_startup.sh
+++ heroku_startup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+export RAILS_ENV=development
+export RUBY_ENV=development
+export RACK_ENV=development
+
 bundle exec rake db:migrate RAILS_ENV=development
 bundle exec puma -C puma.rb

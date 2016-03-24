3a5e1663c596515f7e5f3a55481e82e15570eae7
Hernan Maschwitz <hernanm992@gmail.com>
2016-03-24 19:58:31 -0300

    fix login status code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
     def login
       if (login_params[:username] ==  'admin' && login_params[:password] == 'secret')
-          render json: {message: "ok"}, :status => 202
+          render json: {message: "ok"}, :status => 200
       else
           render json: {message: "Wrong"}, :status => 401
       end
